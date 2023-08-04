5fc80d95cebcfb28c32095a81ab4e5a9d16cebba
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-08-04 14:15:33 -0400

    Make the script work for win=200, sr=4000, num_harmonics=1

--- script.py
+++ script.py
@@ -6,10 +6,13 @@ import pdb
 # adjustable constants
 score_path = './test_files/monophonic1note.mid'
 aFreq = 440
-width = 3
+width = 0
 bpm = 60
-winms = 100
-targetsr = 2000
+winms = 200
+sample_rate = 4000
+base_note = 1
+tuning_factor = 1
+num_harmonics = 1
 
 # basic indexing of score
 score = m21.converter.parse(score_path)
@@ -50,7 +53,7 @@ midi_pitches = m21ObjectsNoTies.applymap(_midiPitchHelper, na_action='ignore')
 midi_pitches = midi_pitches.ffill().astype(int)
 if not all([rest == -1 for rest in midi_pitches.iloc[-1, :]]):
     midi_pitches.loc[score.highestTime, :] = -1   # add "rests" to end if the last row is not already all rests
-    
+
 # construct midi piano roll / mask, NB: there are 128 possible midi pitches
 _piano_roll = pd.DataFrame(index=range(128), columns=midi_pitches.index.values)
 def _reshape(row):
@@ -84,11 +87,32 @@ piano_roll = _piano_roll.ffill(axis=1).fillna(0).astype(int)
 col_set = set(piano_roll.columns)  # this set makes sure that any timepoints in piano_roll cols will persist
 slices = 60/bpm * 20
 col_set.update([t/slices for t in range(0, int(piano_roll.columns[-1] * slices) + 1)])
-num_rows = int(2 ** round(math.log(winms / 1000 * targetsr) / math.log(2) - 1)) + 1
+num_rows = int(2 ** round(math.log(winms / 1000 * sample_rate) / math.log(2) - 1)) + 1
 sampled = pd.DataFrame(columns=sorted(col_set), index=range(num_rows))
 sampled.update(piano_roll)
 sampled = sampled.ffill(axis=1).fillna(0).astype(int)
-# pdb.set_trace()
+
+width_semitone_factor = 2**(width/6)  # use 6 instead of 12 since it is applied above and below
+mask = sampled * 0
+for row in range(base_note - 1, base_note + 127):    # there are 128 distinct midi_pitches 
+  note = base_note - 1 + row
+  freq = tuning_factor * 2**(note/12) * aFreq / 2**(69/12)
+  if sum(sampled.iloc[row]) > 0:
+    row_mask = sampled.loc[row] > 0
+    for harmonic in range(2, num_harmonics + 2):
+      min_freq = 1 + math.floor(harmonic * freq / width_semitone_factor / sample_rate * num_rows)
+      max_freq = 1 + math.ceil(harmonic * freq * width_semitone_factor / sample_rate * num_rows)
+      print({'note':note, 'freq':freq, 'harmonic':harmonic, 'min':min_freq, 'max':max_freq})
+      if min_freq <= num_rows:
+        max_freq = min(max_freq, num_rows)
+        mask.loc[min_freq:max_freq, row_mask] = 1
+
+
+print(mask.loc[100:150, :])
+m2 = mask.replace(0, pd.NA).dropna(how='all')
+print(m2)
+pdb.set_trace()
+
 if width > 0:
   sampled = sampled.replace(0, pd.NA).ffill(limit=width).bfill(limit=width).fillna(0)
 test = sampled.iloc[125:135, 60:90].copy()

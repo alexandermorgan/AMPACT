69fe9954fc931609bd926a209ac81e0fe98c1768
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-10-06 10:36:39 -0400

    Only import Score from script.py

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,5 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from script import *
+from script import Score
 
 def test_sampled_piano_roll(score_path, truth_path):
   print(f'Running test_sampled_piano_roll on {score_path} and {truth_path} ...')

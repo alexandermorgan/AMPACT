b005726f846fe5e16b1a8e3e482b066a392210d3
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-10-06 05:10:20 -0400

    Make Score.analyses private -> Score._analyses

--- script.py
+++ script.py
@@ -24,7 +24,7 @@ class Score:
     self.part_streams = self.score.getElementsByClass(m21.stream.Part)
     self.semi_flat_parts = [part.semiFlat for part in self.part_streams]
     self.part_names = []
-    self.analyses = {}
+    self._analyses = {}
     for i, part in enumerate(self.semi_flat_parts):
       name = part.partName if (part.partName and part.partName not in self.part_names) else 'Part_' + str(i + 1)
       self.part_names.append(name)
@@ -86,7 +86,7 @@ class Score:
           res = joined.iloc[:, 2:].copy()  # get all the columns from the third to the end. Usually just 1 col except for cdata
           res.index = joined['Offset']
           res.index.name = ''
-          self.analyses[(spine.spineType, 0)] = res
+          self._analyses[(spine.spineType, 0)] = res
           if spine.spineType == 'harm' and len(keyVals):
             keyName = 'harmKeys'
             df3 = pd.DataFrame({keyName: keyVals}, index=keyPositions)
@@ -94,36 +94,36 @@ class Score:
             df3 = joined.iloc[:, 2:].copy()
             df3.index = joined['Offset']
             df3.index.name = ''
-            self.analyses[(keyName, 0)] = df3
+            self._analyses[(keyName, 0)] = df3
 
-      if ('function', 0) not in self.analyses:
-        self.analyses[('function', 0)] = pd.Series()
-      if ('harm', 0) not in self.analyses:
-        self.analyses[('harm', 0)] = pd.Series()
-      if ('harmKeys', 0) not in self.analyses:
-        self.analyses[('harmKeys', 0)] = pd.Series()
+      if ('function', 0) not in self._analyses:
+        self._analyses[('function', 0)] = pd.Series()
+      if ('harm', 0) not in self._analyses:
+        self._analyses[('harm', 0)] = pd.Series()
+      if ('harmKeys', 0) not in self._analyses:
+        self._analyses[('harmKeys', 0)] = pd.Series()
   
   def m21_objects(self):
-    if 'm21_objects' not in self.analyses:
-      self.analyses['m21_objects'] = pd.concat(self.parts, axis=1, sort=True)
-    return self.analyses['m21_objects']
+    if 'm21_objects' not in self._analyses:
+      self._analyses['m21_objects'] = pd.concat(self.parts, axis=1, sort=True)
+    return self._analyses['m21_objects']
   
   def lyrics(self):
-    if 'lyrics' not in self.analyses:
-      self.analyses['lyrics'] = self.m21_objects().applymap(lambda cell: cell.lyric or np.nan, na_action='ignore').dropna(how='all')
-    return self.analyses['lyrics']
+    if 'lyrics' not in self._analyses:
+      self._analyses['lyrics'] = self.m21_objects().applymap(lambda cell: cell.lyric or np.nan, na_action='ignore').dropna(how='all')
+    return self._analyses['lyrics']
 
   def _priority(self):
     '''\tFor .krn files, get the line numbers of the events in the piece, which music21
     often calls "priority". For other encoding formats return an empty dataframe.'''
-    if '_priority' in self.analyses:
-      return self.analyses['_priority']
+    if '_priority' in self._analyses:
+      return self._analyses['_priority']
     if self.file_extension != 'krn':
       priority = pd.DataFrame()
     else:
       priority = self.m21_objects().applymap(lambda cell: cell.priority, na_action='ignore').ffill(axis=1).iloc[:, -1].astype(int)
       priority = pd.DataFrame({'Priority': priority.values, 'Offset': priority.index})
-    self.analyses['_priority'] = priority
+    self._analyses['_priority'] = priority
     return priority
 
   def _reindex_like_sampled(self, df, bpm=60, obs=20):
@@ -148,10 +148,10 @@ class Score:
     same bpm as that passed. If bpm is set to 0, it will return the key observations from
     the **harm spine at their original offsets.'''
     key = ('harmKeys', bpm)
-    if key not in self.analyses:
-      harmKeys = self.analyses[('harmKeys', 0)]
-      self.analyses[key] = self._reindex_like_sampled(harmKeys, bpm)
-    return self.analyses[key]
+    if key not in self._analyses:
+      harmKeys = self._analyses[('harmKeys', 0)]
+      self._analyses[key] = self._reindex_like_sampled(harmKeys, bpm)
+    return self._analyses[key]
 
   def harmonies(self, bpm=60):
     '''\tGet the harmonic analysis from the **harm spine as a pandas series if this
@@ -160,10 +160,10 @@ class Score:
     same bpm as that passed. If bpm is set to 0, it will return the **harm spine
     observations at their original offsets.'''
     key = ('harm', bpm)
-    if key not in self.analyses:
-      harm = self.analyses[('harm', 0)]
-      self.analyses[key] = self._reindex_like_sampled(harm, bpm)
-    return self.analyses[key]
+    if key not in self._analyses:
+      harm = self._analyses[('harm', 0)]
+      self._analyses[key] = self._reindex_like_sampled(harm, bpm)
+    return self._analyses[key]
 
   def functions(self, bpm=60):
     '''\tGet the functional analysis from the **function spine as a pandas series if this
@@ -172,10 +172,10 @@ class Score:
     same bpm as that passed. If bpm is set to 0, it will return the **function spine
     observations at their original offsets.'''
     key = ('function', bpm)
-    if key not in self.analyses:
-      functions = self.analyses[('function', 0)]
-      self.analyses[key] = self._reindex_like_sampled(functions, bpm)
-    return self.analyses[key]
+    if key not in self._analyses:
+      functions = self._analyses[('function', 0)]
+      self._analyses[key] = self._reindex_like_sampled(functions, bpm)
+    return self._analyses[key]
 
   def _remove_tied(self, noteOrRest):
     if hasattr(noteOrRest, 'tie') and noteOrRest.tie is not None and noteOrRest.tie.type != 'start':
@@ -183,13 +183,13 @@ class Score:
     return noteOrRest
 
   def m21ObjectsNoTies(self):
-    if 'm21ObjectsNoTies' not in self.analyses:
-      self.analyses['m21ObjectsNoTies'] = self.m21_objects().applymap(self._remove_tied).dropna(how='all')
-    return self.analyses['m21ObjectsNoTies']
+    if 'm21ObjectsNoTies' not in self._analyses:
+      self._analyses['m21ObjectsNoTies'] = self.m21_objects().applymap(self._remove_tied).dropna(how='all')
+    return self._analyses['m21ObjectsNoTies']
 
   def durations(self):
     '''\tReturn dataframe of durations of note and rest objects in piece.'''
-    if 'durations' not in self.analyses:
+    if 'durations' not in self._analyses:
       mp = self.midi_pitches()
       sers = []
       for col in range(len(mp.columns)):
@@ -201,18 +201,18 @@ class Score:
         vals.append(self.score.highestTime - part.index[-1])
         sers.append(pd.Series(vals, part.index))
       df = pd.concat(sers, axis=1)
-      self.analyses['durations'] = df
+      self._analyses['durations'] = df
       df.columns = mp.columns
-    return self.analyses['durations']
+    return self._analyses['durations']
 
   def midi_pitches(self):
     '''\tProcess notes as midi pitches. Midi does not have a representation
     for rests, so use -1 as a placeholder.'''
-    if 'midi_pitches' not in self.analyses:
+    if 'midi_pitches' not in self._analyses:
       midi_pitches = self.m21ObjectsNoTies().applymap(lambda noteOrRest: -1 if noteOrRest.isRest else noteOrRest.pitch.midi, na_action='ignore')
       midi_pitches = midi_pitches.ffill().astype(int)
-      self.analyses['midi_pitches'] = midi_pitches
-    return self.analyses['midi_pitches']
+      self._analyses['midi_pitches'] = midi_pitches
+    return self._analyses['midi_pitches']
 
   def nmats(self, bpm=60):
     '''\tReturn a dictionary of dataframes, one for each voice, each with the following
@@ -226,7 +226,7 @@ class Score:
     DURATION_BEATS columns respectively. The proportion used is determined by the `bpm`
     argument.'''
     key = ('nmats', bpm)
-    if key not in self.analyses:
+    if key not in self._analyses:
       nmats = {}
       dur = self.durations()
       mp = self.midi_pitches()
@@ -241,38 +241,38 @@ class Score:
         df = pd.concat([onsetBeat, durBeat, part, midi, onsetSec, offsetSec], axis=1)
         df.columns = ['ONSET_BEAT', 'DURATION_BEAT', 'PART', 'MIDI', 'ONSET_SEC', 'OFFSET_SEC']
         nmats[partName] = df
-      self.analyses[key] = nmats
-    return self.analyses[key]
+      self._analyses[key] = nmats
+    return self._analyses[key]
 
   def piano_roll(self):
     '''\tConstruct midi piano roll. NB: there are 128 possible midi pitches.'''
-    if 'piano_roll' not in self.analyses:
+    if 'piano_roll' not in self._analyses:
       piano_roll = pd.DataFrame(index=range(128), columns=self.midi_pitches().index.values)
       for offset in self.midi_pitches().index:
         for pitch in self.midi_pitches().loc[offset]:
           if pitch >= 0:
             piano_roll.at[pitch, offset] = 1
       piano_roll.fillna(0, inplace=True)
-      self.analyses['piano_roll'] = piano_roll
-    return self.analyses['piano_roll']
+      self._analyses['piano_roll'] = piano_roll
+    return self._analyses['piano_roll']
 
   def sampled(self, bpm=60, obs=20):
     '''\tSample the score according to bpm, and the desired observations per second, `obs`.'''
     key = ('sampled', bpm, obs)
-    if key not in self.analyses:
+    if key not in self._analyses:
       slices = 60/bpm * obs
       timepoints = pd.Index([t/slices for t in range(0, int(self.score.highestTime * slices))])
       pr = self.piano_roll().copy()
       pr.columns = [col if col in timepoints else timepoints.asof(col) for col in pr.columns]
       sampled = pr.reindex(columns=timepoints, method='ffill')
-      self.analyses[key] = sampled
-    return self.analyses[key]
+      self._analyses[key] = sampled
+    return self._analyses[key]
 
   def mask(self, winms=100, sample_rate=2000, num_harmonics=1, width=0,
             bpm=60, aFreq=440, base_note=0, tuning_factor=1, obs=20):
     '''\tConstruct a mask from the sampled piano roll using width and harmonics.'''
     key = ('mask', winms, sample_rate, num_harmonics, width, bpm, aFreq, base_note, tuning_factor)
-    if key not in self.analyses:
+    if key not in self._analyses:
       width_semitone_factor = 2 ** ((width / 2) / 12)
       sampled = self.sampled(bpm, obs)
       num_rows = int(2 ** round(math.log(winms / 1000 * sample_rate) / math.log(2) - 1)) + 1
@@ -292,8 +292,8 @@ class Score:
               maxbin = min(maxbin, num_rows)
               mcol.loc[minbin : maxbin] = 1
           mask.iloc[np.where(mcol)[0], np.where(sampled.iloc[row])[0]] = 1
-      self.analyses[key] = mask
-    return self.analyses[key]
+      self._analyses[key] = mask
+    return self._analyses[key]
 
 
 # piece = Score(score_path='./test_files/M025_00_01a_a-repeated.krn')
@@ -310,7 +310,7 @@ class Score:
 # mask = piece.mask()
 # harm = piece.harmonies()
 # functions = piece.functions()
-# cdata = piece.analyses[('cdata', 0)]
+# cdata = piece._analyses[('cdata', 0)]
 # h2 = harm[harm != harm.shift()]
 # f2 = functions[functions != functions.shift()]
 # df = pd.concat([f2, h2, cdata], axis=1, sort=True).ffill()
